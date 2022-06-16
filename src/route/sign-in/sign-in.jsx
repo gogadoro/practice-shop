4d19ffdbedacf355d3efab82637e889ad21f44aa
gogadoro <gogadoro92@gmail.com>
2022-06-17 02:57:06 +0900

    쉽지않다.. 그래도 리디렉션이랑 오류찾으면서 하나씩하나씩 좋다.

--- src/route/sign-in/sign-in.jsx
+++ src/route/sign-in/sign-in.jsx
@@ -1,20 +1,48 @@
-import { signIn_GooglePopup } from "../../utils/firebase/firebase";
+import { useEffect } from 'react';
+import { getRedirectResult } from "firebase/auth";
+
+import {
+    signIn_GooglePopup,
+    setUserDbFromAuth,
+    auth,
+    signIn_GoogleRedirect
+} from "../../utils/firebase/firebase";
 
 const SignIn = () => {
 
+    
+    useEffect( () => {
+        // useEffect fst param 으로는 동기적 함수가 와야한다고 해서 이너함수를 만듬.
+        const fakeFunction = async () => {
+            const response = await getRedirectResult(auth);
+            
+            if (response) {
+                const userDocRef = await setUserDbFromAuth(response.user);
+            } else {
+                return;
+            }
+        }
+        fakeFunction();
+    }, []);
+    
+
     const logUser_Google = async () => {
-        const response = await signIn_GooglePopup();
-        console.log(response);
+        const { user } = await signIn_GooglePopup();
+        const userDocRef = await setUserDbFromAuth(user);
     }
+
 
     return (
         <div>
-            <h1>로그인 , 회원가입</h1>
+            <h1>Login , Sign In</h1>
             <button onClick={logUser_Google}>
-                    Login with Google
+                Login with Google
+            </button>
+            <button onClick={signIn_GoogleRedirect}>
+                Login with Google Redirect
             </button>
         </div>
     )
 }
 
-export default SignIn;
+export default SignIn; 
--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -1,9 +1,15 @@
-import { InitializeApp } from 'firebase/app';
-import { getAuth, 
-        singInWithReDirection, 
-        singInWithPopup, 
-        GoogleAuthPorvider } 
-        from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { 
+    getAuth, 
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider } from 'firebase/auth'
+
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc, } from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCR8wdcaCzNTJHa9yoOST241p3hOOD8Fpo",
@@ -14,9 +20,36 @@ const firebaseConfig = {
     appId: "1:631247507579:web:cb6839dc34037bffbed2de"
 };
 
-const firebaseApp = InitializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 
-const GoogleProvider = new GoogleAuthPorvider();
+const GoogleProvider = new GoogleAuthProvider();
 
 export const auth = getAuth();
-export const signIn_GooglePopup = () => singInWithPopup(auth, GoogleProvider)
+export const signIn_GooglePopup = () => signInWithPopup(auth, GoogleProvider)
+export const signIn_GoogleRedirect = () => signInWithRedirect(auth, GoogleProvider)
+
+
+export const db = getFirestore();
+
+export const setUserDbFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log('docRef :', userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef)
+    console.log('snapshot :', userSnapshot);
+    console.log(userSnapshot.exists());
+
+    if (!userSnapshot.exists()) {
+        const email = userAuth.email;
+        const displayName = userAuth.displayName;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {email, displayName, createdAt});
+        } catch (eer) {
+            console.log('error creating the user', eer.message);
+        }
+    }
+
+    return userDocRef;
+}
